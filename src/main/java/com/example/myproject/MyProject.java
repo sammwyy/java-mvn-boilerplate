2015078c616fad6319bfa777d363cd18d2fc9acf
Sammwy <sammwy.dev@gmail.com>
2021-10-01 13:21:51 -0300

    Initial source

---
+++ src/main/java/com/example/myproject/MyProject.java
@@ -0,0 +1,11 @@
+package com.example.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyProject {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
